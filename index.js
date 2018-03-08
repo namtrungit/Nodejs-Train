e32a59b388727fb5f6ca641c1ec1947fa0779333
Trung <namtrung18101996@gmail.com>
2018-03-08 22:19:34 +0700

    Lessson8: Http Module

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
-// var http = require('http');
-// http.createServer(function (req, res) {
-//     res.writeHead(200, { 'ContenType': 'text/plan' });
-//     res.end('Hello Trung');
-// }).listen(8000);;
+var http = require('http');
+//vd1 
+// var server = http.createServer(function (req, res) {
+//     res.writeHead(200, {
+//         'Content-type': 'text/html'
+//     })
+//     res.write('<h1>Hello World</h1>');
+//     res.end();
+// });
+// server.listen(8000);
+// console.log('Server is running in port 8000');
 
-// console.log('Server chạy ở http://127.0.0.1:8000 or localhost:8000');
-
-var logger = require('./module/log');
-logger.info('Trung');
-logger.error('Trung noob');
-logger.success('Trung is master in CSGO');
+//Url
+var server = http.createServer(function (req, res) {
+    if (req.url === '/') {
+        res.writeHead(200, {
+            'Content-type': 'text/html'
+        })
+        res.write('<h1>This is home Page</h1>');
+        res.end();
+    }
+    if(req.url === '/student'){
+        res.writeHead(200,{
+            'Content-Type':'text/html'
+        })
+        res.write(`<h2 style='color:red'>This is student page</h2>`)
+        res.end();
+    }
+    else{
+        res.writeHead(200,{
+            'Content-Type':'text/html'
+        })
+        res.write(`<h2>This is normal page</h2>`)
+        res.end();
+    }
+});
+server.listen(8000);
+console.log('Server is running in port 8000');
